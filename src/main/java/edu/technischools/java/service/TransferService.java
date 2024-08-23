a9d2a11914d829935bad36e3a74153e80bb4da8d
magnolia1de <saturmag@gmail.com>
2024-08-23 13:51:11 +0200

    po polsku komentarze

--- src/main/java/edu/technischools/java/service/TransferService.java
+++ src/main/java/edu/technischools/java/service/TransferService.java
@@ -8,7 +8,7 @@ public class TransferService {
 
     public void transfer(Account fromAccount, Account toAccount, double amount) throws InsufficientFundsException {
         if (fromAccount.getBalance().compareTo(BigDecimal.valueOf(amount)) < 0) {
-            throw new InsufficientFundsException("Insufficient balance for transfer");
+            throw new InsufficientFundsException("Niewystarczające środki na koncie do wykonania przelewu");
         }
 
         fromAccount.debit(amount);
